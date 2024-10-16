af7e45bf2ad29875258c18d58989edeba369cdb6
CodePawsDev <isripen1991@outlook.com>
2024-10-16 16:51:21 +0700

    feat: add initial exercises in thai language

---
+++ th/ex-1.js
@@ -0,0 +1,13 @@
+/* 
+ให้เขียน Function ที่ชื่อว่า sortedFromMinToMax ซึ่งมีคุณสมบัติต่อไปนี้
+    - รับ Parameter 1 ตัวคือ numbers
+        1. numbers เป็น Array ที่บรรจุตัวเลขที่ยังไม่เรียงลำดับ
+    - Function นี้จะเรียงข้อมูลใน numbers จากน้อยไปมาก โดยใช้ Bubble Sort Algorithm และ Return numbers ออกมา
+    - Function นี้จะนำตัวเลขแต่ละตัวที่บรรจุไว้ใน matrix มาคูณ 2 และบรรจุลงใน Array ใหม่จากนั้นจะ Return ค่า Array ใหม่ออกมา
+    - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ด้านล่าง
+*/
+
+// Start coding here
+
+const numbers = [64, 34, 25, 12, 22, 11, 90];
+console.log(sortedFromMinToMax(numbers)); // [11, 12, 22, 25, 34, 64, 90]
---
+++ th/ex-2.js
@@ -0,0 +1,27 @@
+/* 
+ให้เขียน Function ที่ชื่อว่า sortedByScore ซึ่งมีคุณสมบัติต่อไปนี้
+    - รับ Parameter 1 ตัวคือ studentsInfo
+        1. studentsInfo เป็น Array ของ Objects ที่บรรจุข้อมูลนักเรียน โดย Object แต่ละอันจะมี Key 3 ตัวได้แก่ firstname , lastname และ score 
+    - Function นี้จะเรียงลำดับ Object ภายใน Array ตามคะแนนสอบจากมากไปน้อย โดยใช้ Bubble Sort Algorithm และ Return studentsInfo ออกมา
+    - เมื่อ Algorithm นี้ทำงาน จะต้องเห็นผลลัพธ์ดังนี้
+*/
+
+// Start coding here
+
+const studentsInfo = [
+  { firstname: "John", lastname: "Doe", score: 85 },
+  { firstname: "Jane", lastname: "Smith", score: 92 },
+  { firstname: "James", lastname: "Johnson", score: 78 },
+  { firstname: "Emily", lastname: "Davis", score: 88 },
+];
+
+const result = sortedByScore(studentsInfo);
+console.log(result);
+/*
+  [
+    { firstname: 'Jane', lastname: 'Smith', score: 92 },
+    { firstname: 'Emily', lastname: 'Davis', score: 88 },
+    { firstname: 'John', lastname: 'Doe', score: 85 },
+    { firstname: 'James', lastname: 'Johnson', score: 78 }
+  ]
+  */
