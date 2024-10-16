a3111843ec55a7e72655f56176120393858f8986
CodePawsDev <isripen1991@outlook.com>
2024-10-16 16:54:53 +0700

    feat: add answer to exercise in thai language

--- th/ex-1.js
+++ th/ex-1.js
@@ -9,5 +9,20 @@
 
 // Start coding here
 
+function sortedFromMinToMax() {
+  const n = numbers.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        let temp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = temp;
+      }
+    }
+  }
+  return numbers;
+}
+
 const numbers = [64, 34, 25, 12, 22, 11, 90];
 console.log(sortedFromMinToMax(numbers)); // [11, 12, 22, 25, 34, 64, 90]
